3f23d7fb9f58e681064aed481c20f53913bd0469
LokeshrajURK24CS1276 <lokeshrajm@karunya.edu.in>
2025-08-13 21:55:46 +0530

    Update q8.java

--- exp2/src/q8.java
+++ exp2/src/q8.java
@@ -1,33 +1,28 @@
 import java.util.Scanner;
-
 public class q8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter 10 numbers (space-separated):");
-        String input = sc.nextLine(); // Read the whole line
-        String[] tokens = input.split(" "); // Split by spaces
+        int[] arr = new int[10]; 
 
-        int[] arr = new int[10];
+        
+        System.out.println("Enter 10 numbers:");
         for (int i = 0; i < 10; i++) {
-            arr[i] = Integer.parseInt(tokens[i]); // Convert each to int
+            arr[i] = sc.nextInt();
         }
-
         System.out.print("Original Array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i] + " ");
         }
 
-        System.out.print("\nOdd Array : ");
-        for (int num : arr) {
-            if (num % 2 != 0)
-                System.out.print((num * num) + " ");
+        System.out.print("\nOdd Array: ");
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print((arr[i] * arr[i]) + " ");
+            }
         }
-
-        System.out.print("\nEven Array : ");
-        for (int num : arr) {
-            if (num % 2 == 0)
-                System.out.print((num * num * num) + " ");
-        }
-    }
-}
+        System.out.print("\nEven Array: ");
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print((arr[i] * arr[i] * arr[i]) + " ");
+            }}}}
